a7683e63de0fa59197f2f27b85b81286794cdd2d
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-04-19 13:10:51 +0000

    Finish: JavaScript_lessons section 26

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
 // 厳密なエラーを発見するもの。とりあえず書いておく。
 'use strict';
 
-// console.log('5' * 3);
-// console.log(5 - '3');
+{
+  const x = 100;
+  console.log(x);
+}
 
-// console.log('5' + 3);
-
-// // 5という文字列を１０進数に変換
-// console.log(parseInt('5', 10) + 3);
-
-console.log(parseInt('hello', 10)); // NaN - Not a Number
+// ブロックで囲う
