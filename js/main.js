acec0a3f345f019004033bbb85d9ae10760c497b
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-04-19 15:10:32 +0000

    Finish: JavaScript_lessons section 2-2

--- js/main.js
+++ js/main.js
@@ -2,10 +2,13 @@
 'use strict';
 
 {
-  // const score1 = 80;
-  // const score2 = 90;
-  // const score3 = 40;
-
   const scores = [80, 90, 40]
-  console.log(scores)
+  // console.log(scores[1]);
+
+  // scores[2] = 44;
+  // console.log(scores);
+
+  // const = 10;
+
+  console.log(scores.length);
 }
