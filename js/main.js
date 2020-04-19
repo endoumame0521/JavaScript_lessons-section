eef05d381d15a7b9dd61bec0c62d7f6c4fc959d6
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-04-19 11:20:46 +0000

    Finish: JavaScript_lessons section 5

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 // 厳密なエラーを発見するもの。とりあえず書いておく。
 'use strict';
 
-// コメント
+// console.log('hello');
+// console.log("hello");
 
-/*
-コメント
-コメント
-コメント
-*/
+// console.log('it's me!');
+// console.log("it's me");
 
-console.log('Hello World from main.js!');
+// \〇〇　：　〇〇は文字列として扱われる
+// console.log('it\'s me');
+
+// \n 改行、\t tab
+// console.log('hel\nlo wor\tld');
+
+// 文字列の連結
+console.log('hello' + 'world');
