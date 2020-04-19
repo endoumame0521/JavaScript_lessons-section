425165aeb2ab3638cb87d726237ccda3c856f5d3
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-04-19 12:53:45 +0000

    Finish: JavaScript_lessons section 8

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 // 厳密なエラーを発見するもの。とりあえず書いておく。
 'use strict';
 
-// console.log(10 + 3); // 13
-// console.log(10 - 3); // 7
-// console.log(10 * 3); // 30
-// console.log(10 / 3); //3.333
-// console.log(10 % 3); // 1
-// console.log(10 ** 3); // 1000
+//定数 const
+//変数 let var　varは古いので使用しない
 
-// 演算子
-// 優先度
+// console.log(150 * 140);
+// console.log(150 * 160);
 
-console.log(2 + 10 * 3); //32
-console.log((2 + 10) * 3); //36
+let price = 150;
+console.log(price * 140);
+console.log(price * 160);
+
+price = 170;
+console.log(price * 140);
+console.log(price * 160);
+
