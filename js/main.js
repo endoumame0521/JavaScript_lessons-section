61be90c13556295b8beac4173317205b0eb45e25
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-04-19 15:21:53 +0000

    Finish: JavaScript_lessons section 2-5

--- js/main.js
+++ js/main.js
@@ -3,15 +3,7 @@
 
 {
   const scores = [80, 90, 40, 70];
-  scores.push(60, 50);
-  scores.shift();
-  // 90, 40, 70. 60, 50
-
-  // console.log(`Score: ${scores[0]}`);
-  // console.log(`Score: ${scores[1]}`);
-  // console.log(`Score: ${scores[2]}`);
-
-  // for (let i = 0; i < 3; i++) {
+  scores.splice(1, 1, 40, 50)
   for (let i = 0; i < scores.length; i++) {
     console.log(`Score ${i}: ${scores[i]}`);
   }
