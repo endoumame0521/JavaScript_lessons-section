d697b460049c149c9a0f380ea767df98cf98f05c
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-04-20 12:57:02 +0900

    Finish: JavaScript_lessons section 3-2

--- js/main.js
+++ js/main.js
@@ -3,43 +3,11 @@
 
 // カプセル化
 {
-  // クラスの定義
-  class Post { // 親クラス
-    constructor(text) {
-      this.text = text;
-      this.likeCount = 0;
-    }
-    // メソッド
-    show() {
-      console.log(`${this.text} - ${this.likeCount} likes`);
-    }
-
-    like() {
-      this.likeCount++
-      this.show();
-    }
+  function update() {
+    // document.querySelector('h1').textContent = 'Changed!';
+    // document.querySelector('#target').textContent = 'Changed!';
+    document.getElementById('target').textContent = 'Changed!';
   }
 
-  // 親クラス（Post）を継承
-  class SponsoredPost extends Post { // 子クラス
-    constructor(text, sponsor) {
-      super(text);
-      this.sponsor = sponsor;
-    }
-    // メソッド
-    show() {
-      super.show();
-      console.log(`...sponsored by ${this.sponsor}`);
-    }
-  }
-
-  const posts = [
-    // インスタンスの生成
-    new Post('JavaScriptの勉強中・・・'),
-    new Post('プログラミング楽しい！'),
-    new SponsoredPost('2分どうがでマスターしよう', 'dotinstall'),
-  ];
-
-  posts[2].show();
-  posts[2].like();
+  setTimeout(update, 1000);
 }
