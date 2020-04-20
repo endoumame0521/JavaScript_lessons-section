24aeedb363b4fac80f50a7d1331bf53d47038a51
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-04-20 10:35:39 +0900

    Finish: JavaScript_lessons section 2-27

--- js/main.js
+++ js/main.js
@@ -2,5 +2,15 @@
 'use strict';
 
 {
+  // const name = 'taguchi';
+  const name = 5;
 
+  // try {} で囲った箇所は、エラーが発生しても、そこで止まることはない。
+  try {
+    console.log(name.toUpperCase());
+  } catch (e) {
+    console.log(e);
+  }
+
+  console.log('Finish');
 }
