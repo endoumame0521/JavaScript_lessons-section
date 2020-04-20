b4ea09f246ad892004f1417bd067925c71aca7c4
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-04-20 10:24:36 +0900

    Finish: JavaScript_lessons section 2-24

--- js/main.js
+++ js/main.js
@@ -2,11 +2,19 @@
 'use strict';
 
 {
-  // alert('hello');
-  const answer = confirm('削除しますか？');
-  if (answer) {
-    console.log('削除しました');
-  } else {
-    console.log('キャンセルしました');
+  let i = 0;
+
+  function showTime() {
+    console.log(new Date());
+    i++;
+    if (i >2) {
+      // この関数が3回実行されたら終了させる
+      clearInterval(intervalId);
+    }
   }
+
+  // 関数そのものを100ms毎に実行し続ける
+  // 注意）関数の（）は要らない。
+  // （）を入れてしまうと、関数の返り値を繰り返し処理してしまう。
+  const intervalId = setInterval(showTime, 1000);
 }
