946d57f4a8662ae2223ad1473c6fdbee520977fe
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-04-20 13:16:20 +0900

    Finish: JavaScript_lessons section 3-6

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
 // 厳密なエラーを発見するもの。とりあえず書いておく。
 'use strict';
 
-// イベント
 {
   document.querySelector('button').addEventListener('click', () => {
     const targetNode =  document.getElementById('target');
-    targetNode.textContent = 'Changed!';
-    targetNode.title = 'This is title!';
-    targetNode.style.color = 'red';
-    targetNode.style.backgroundColor = 'skyblue';
+
+    targetNode.className = 'my-color my-border';
   });
 }
