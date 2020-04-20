7ce65ae79e2fadf5f3e6cd4390487ff146eb7eec
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-04-20 11:30:45 +0900

    Finish: JavaScript_lessons section 2-32

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // 厳密なエラーを発見するもの。とりあえず書いておく。
 'use strict';
 
-
+// カプセル化
 {
   // クラスの定義
   class Post {
@@ -12,7 +12,12 @@
 
     // メソッド
     show() {
-      console.log(`${this.text} - ${this.likeCount}いいね`);
+      console.log(`${this.text} - ${this.likeCount} likes`);
+    }
+
+    like() {
+      this.likeCount++
+      this.show();
     }
   }
 
@@ -22,6 +27,7 @@
     new Post('プログラミング楽しい！'),
   ];
 
-  posts[0].show();
-  posts[1].show();
+  posts[0].like();
+  // posts[0].show();
+  // posts[1].show();
 }
