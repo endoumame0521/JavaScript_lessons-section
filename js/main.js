b6d11891c2694ef078bf4e9e7bc8cb9e3ef5b7cf
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-04-20 11:25:20 +0900

    Finish: JavaScript_lessons section 2-31

--- js/main.js
+++ js/main.js
@@ -1,34 +1,27 @@
 // 厳密なエラーを発見するもの。とりあえず書いておく。
 'use strict';
 
-// オブジェクトの中のプロパティに関数を入れると、その関数はメソッドと呼ばれる
-// そして、メソッドチェインで呼び出せる。
-// また、このようなメソッドの場合はfunctionという表記を省略する事もできる。
 
 {
+  // クラスの定義
+  class Post {
+    constructor(text) {
+      this.text = text;
+      this.likeCount = 0;
+    }
+
+    // メソッド
+    show() {
+      console.log(`${this.text} - ${this.likeCount}いいね`);
+    }
+  }
+
   const posts = [
-    {
-      text: 'JavaScriptの勉強中・・・',
-      likeCount: 0,
-      // show: function () {
-      //   console.log(`${this.text} - ${this.likeCount}いいね`);
-      // },
-      show() {
-        console.log(`${this.text} - ${this.likeCount}いいね`);
-      },
-    },
-    {
-      text: 'プログラミング楽しい！',
-      likeCount: 0,
-      show() {
-        console.log(`${this.text} - ${this.likeCount}いいね`);
-      },
-    },
+    // インスタンスの生成
+    new Post('JavaScriptの勉強中・・・'),
+    new Post('プログラミング楽しい！'),
   ];
 
-
-
-  // show(posts[0]);
   posts[0].show();
   posts[1].show();
 }
