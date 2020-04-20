4c5dace2e5c150e05768193736322e61518ae71f
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-04-20 10:31:26 +0900

    Finish: JavaScript_lessons section 2-26

--- js/main.js
+++ js/main.js
@@ -2,16 +2,5 @@
 'use strict';
 
 {
-  let i = 0;
 
-  function showTime() {
-    console.log(new Date());
-    const timeoutId = setTimeout(showTime, 1000);
-    i++;
-    if (i > 2) {
-      clearTimeout(timeoutId);
-    }
-  }
-
-  showTime();
 }
