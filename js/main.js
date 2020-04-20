1ff72f558c43b15c42286245315e59febec63689
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-04-20 13:49:35 +0900

    Finish: JavaScript_lessons section 3-13

--- js/main.js
+++ js/main.js
@@ -2,14 +2,11 @@
 'use strict';
 
 {
-  // 入力フォームの操作
+  // セレクトボックスの操作
   document.querySelector('button').addEventListener('click', () => {
     const li = document.createElement('li');
-    const text = document.querySelector('input');
-    li.textContent = text.value;
+    const color = document.querySelector('select');
+    li.textContent = `${color.value} - ${color.selectedIndex}`;
     document.querySelector('ul').appendChild(li);
-
-    text.value = '';
-    text.focus();
   });
 }
