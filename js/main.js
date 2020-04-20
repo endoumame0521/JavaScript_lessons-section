7ef66ddce0ba06e4b491fdba17b4208793728c0f
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-04-20 13:24:49 +0900

    Finish: JavaScript_lessons section 3-8

--- js/main.js
+++ js/main.js
@@ -5,13 +5,8 @@
   document.querySelector('button').addEventListener('click', () => {
     const targetNode =  document.getElementById('target');
 
-    // targetNode.className = 'my-color my-border';
-    // targetNode.classList.add('my-color');
-    // if (targetNode.classList.contains('my-color') === true) {
-    //   targetNode.classList.remove('my-color');
-    // } else {
-    //   targetNode.classList.add('my-color');
-    // }
-    targetNode.classList.toggle('my-color');
+    // カスタムデータ属性
+    // targetNode.textContent = 'Dotinstall';
+    targetNode.textContent = targetNode.dataset.translation;
   });
 }
