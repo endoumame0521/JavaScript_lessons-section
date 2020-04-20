c374e971eb6309cabd3c21395a5874394c4cbdbb
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-04-20 13:08:07 +0900

    Finish: JavaScript_lessons section 3-4

--- js/main.js
+++ js/main.js
@@ -1,16 +1,9 @@
 // 厳密なエラーを発見するもの。とりあえず書いておく。
 'use strict';
 
-// カプセル化
+// イベント
 {
-  function update() {
-    // document.getElementById('target').textContent = 'Changed!';
-    // document.querySelector('p').textContent = 'Changed!';
-    // document.querySelectorAll('p')[1].textContent = 'Changed!';
-    document.querySelectorAll('p').forEach((p, index) => {
-      p.textContent = `${index}番目のpです！`;
-    });
-  }
-
-  setTimeout(update, 1000);
+  document.querySelector('button').addEventListener('click', () => {
+    document.getElementById('target').textContent = 'Changed!';
+  });
 }
