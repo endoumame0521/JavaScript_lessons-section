fdc7ea469dacf21cfd9b7624ece7214e4b2fd266
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-04-20 10:04:49 +0900

    Finish: JavaScript_lessons section 2-20

--- js/main.js
+++ js/main.js
@@ -2,23 +2,10 @@
 'use strict';
 
 {
-  const scores = [10, 3, 9];
+  // 0以上1未満のランダムな数値を発生
+  // console.log(Math.random());
 
-  let sum = 0;
+  // 1から6の自然数をランダムに発生
+  console.log(Math.floor(Math.random() * 6) + 1);
 
-  scores.forEach(score => {
-    sum += score;
-  });
-
-  const avg = sum / scores.length;
-
-  // console.log(sum);
-  // console.log(avg);
-
-  // console.log(Math.floor(avg)); // 7　小数点以下切り捨て
-  // console.log(Math.ceil(avg)); // 8　小数点以下切り上げ
-  // console.log(Math.round(avg)); // 7　四捨五入
-  // console.log(avg.toFixed(3)); // 7.333　指定した桁数のみ表示
-
-  console.log(Math.random()); // 0以上1未満のランダムな数値を発生
 }
